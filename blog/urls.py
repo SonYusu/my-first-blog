21936d342b754ef45ede92ac13c1472f11f33beb
joker <jin19982@gmail.com>
2018-08-25 22:26:26 +0900

    blog template complete

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:question_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:question_id>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/edit/<int:question_id>/', views.post_edit, name='post_edit'),
 ]
